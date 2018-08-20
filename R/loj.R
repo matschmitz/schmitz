66c66e150217f743017600cbce4c516a369dc019
matschmitz <37771001+matschmitz@users.noreply.github.com>
2018-08-20 10:38:46 +0200

    separate functions

--- R/loj.R
+++ R/loj.R
@@ -8,17 +8,6 @@
 #' @examples NULL
 #' @export loj
 loj <- function(X = NULL, Y = NULL, onCol = NULL) {
-    giveExemple <- is.null(X) & is.null(Y) & is.null(onCol)
-    if (giveExemple) {
-        cat("\nExemple:")
-        cat("\n>X\n")
-        X <- data.table(id = 1:5, L = letters[1:5]) %T>% print
-        cat("\n>Y\n")
-        Y <- data.table(id = 3:5, L = c(NA, "g", "h"), N = c(10, NA, 12)) %T>% print
-        onCol <- "id"
-        cat('\nLOJ(X, Y, "id")\n')
-    }
-    n <- names(Y)
     X[Y, (n) := mget(paste0("i.", n)), on = onCol]
     if (giveExemple) {cat(">X\n"); print(X)}
 }
