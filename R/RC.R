99bc1c3eb6c698a4b5651c29400f06f94e2e722a
matschmitz <37771001+matschmitz@users.noreply.github.com>
2018-08-26 12:15:10 +0200

    print CI

--- R/RC.R
+++ R/RC.R
@@ -1,8 +1,12 @@
+#' @importFrom png writePNG readPNG
+#' @importFrom magick image_read
+
 #' @title Generate classification image
 #' @description Generate the Classification Image (CI) over a given set of responses.
 #' @param res Numerical vector with the reponses.
 #' @param stim Numerical vector specifying the stimuli number.
-#' @param N Matrix of noise pattern as generated with \code{\link[rcicr]{generateStimuli2IFC}}.
+#' @param N Matrix of noise pattern as generated with
+#'   \code{\link[rcicr]{N <- generateStimuli2IFC(..., return_as_dataframe = TRUE)}}.
 #' @return CI
 #' @examples NULL
 #' @export genCI
@@ -43,9 +47,10 @@ scaleCI <- function(CI, baseImg, scaling = "matched") {
     if (is.character(baseImg)) baseImg <- readBaseImg(baseImg)
 
     if (scaling == "matched") {
-        scaledCI <- min(baseImg) + ((max(baseImg) - min(baseImg)) * (CI - min(CI)) / (max(CI) - min(CI)))
+        scaledCI <- min(baseImg) + ((max(baseImg)-min(baseImg))*(CI-min(CI))/(max(CI)-min(CI)))
     } else if (is.numeric(scaling)) { # constant scaling
         scaledCI <- (CI + scaling)/(2 * scaling)
+        if (max(scaledCI) > 1 | max(scaledCI) < -1) warning("Constant is too low! Adjust.")
     } else { # No scaling
         scaledCI <- CI
     }
@@ -56,14 +61,38 @@ scaleCI <- function(CI, baseImg, scaling = "matched") {
 #' @title Generate combined image
 #' @description Generate the combinaed image by imposing the selected noise on the base image.
 #' @inheritParams scaleCI
-#' @param outpath String specifying the output target path
+#' @param outpath String specifying the output target path.
+#' @param resize Scalar specifying if the image should be resized.
 #' @return NULL
 #' @examples NULL
 #' @export genCombinedImg
-genCombinedImg <- function(CI, baseImg, filename = NULL, outpath = "./cis", size = 512) {
-    if (is.null(filename)) filename <- "combined.jpeg"
+genCombinedImg <- function(CI, baseImg,
+                           filename = NULL, outpath = "./cis",
+                           resize = NULL, printImg = FALSE) {
+
+    if (is.character(baseImg)) baseImg <- readBaseImg(baseImg)
+
+    # Default filename
+    if (is.null(filename)) filename <- "combined.png"
+
+    # check dimensions
+    if (dim(CI)[1] != 512) CI <- matrix(CI, nrow = 512)
+    if (dim(baseImg)[1] != 512) CI <- matrix(baseImg, nrow = 512)
+
+    # Write and save combined image
     combined <- (baseImg + CI) / 2
-    path <- file.path(outpath, filename)
-    jpeg::writeJPEG(combined, path)
+    if (!dir.exists(outpath)) dir.create(outpath)
+    imgPath <- file.path(outpath, filename)
+    png::writePNG(combined, imgPath)
+
+    # Resize
+    if (!is.null(resize))  magick::image_read(imgPath) %>%
+        magick::image_scale(resize) %>%
+        magick::image_write(path = imgPath, format = "png")
+
+    # Print image
+    if (printImg) print( magick::image_read(imgPath) )
+
+    return(combined)
 }
 
