232315610e70bea78d0cfe105b39538854478100
Mathias Schmitz <mathias.schmitz@uclouvain.be>
2018-08-27 14:44:00 +0200

    everything works

--- R/RC.R
+++ R/RC.R
@@ -25,69 +25,71 @@ readBaseImg <- function(baseImgPath, maxContrast = TRUE) {
     return( as.vector(baseImg) )
 }
 
-#' @title Generate (scaled) noise mask from responses
-#' @description Generate the noise mask (NM) over a given set of responses.
-#' @param res Numerical vector with the reponses.
+#' @title Generate (and scale) mask from responses
+#' @description Generate (and scale) mask from responses.
+#' @param response Numerical vector with the reponses.
 #' @param stim Numerical vector specifying the stimuli number.
-#' @param N Matrix of noise pattern as generated with
-#'   \code{\link[rcicr]{N <- generateStimuli2IFC(..., return_as_dataframe = TRUE)}}.
-#' @param baseImg Numerical vector containing the baseImg image or string pointing to the baseImg image file.
-#'   If baseImg is a string, then the baseImg image must in .png or .jpeg.
+#' @param noiseMatrix Matrix of noise pattern as generated with
+#'   \code{\link[rcicr]{noiseMatrix <- generateStimuli2IFC(..., return_as_dataframe = TRUE)}}.
+#' @param baseImg Numerical vector containing the baseImg image or string pointing to the baseImg
+#'   image file. If baseImg is a string, then the baseImg image must in .png or .jpeg.
 #' @param scaling String|Scalar|NULL specifying the scaling method. `"matched"` is the default method.
 #'   If a scalar is provided (e.g. 5) than the `"constant"` method will be applied.
 #'   If `NULL` no scaling is applied.
-#' @return (Un)scaled Noise mask (NM).
+#' @return (Un)scaled Noise mask (mask).
 #' @examples NULL
-#' @export genNM
-genNM <- function(res, stim, N, baseImg, scaling = "matched") {
-    # Generate noise mask (NM)
-    X  <- data.table::data.table(res = res, stim = stim)
-    X  <- X[, .(res = mean(res)), stim]
-    NM <- (N[, X$stim] %*% X$res) / length(X$res)
+#' @export genMask
+genMask <- function(response, stim, noiseMatrix, baseImg, scaling = "matched") {
+    # Generate mask
+    X  <- data.table::data.table(response = response, stim = stim)
+    X  <- X[, .(response = mean(response)), stim]
+    mask <- (noiseMatrix[, X$stim] %*% X$response) / length(X$response)
 
     # Read base image
     if (is.character(baseImg)) baseImg <- readBaseImg(baseImg)
 
-    # Scale NM
+    # Scale mask
     if (scaling == "matched") {
-        scaledNM <- min(baseImg)+((max(baseImg)-min(baseImg))*(NM-min(NM))/(max(NM)-min(NM)))
+        scaledMask <- min(baseImg)+((max(baseImg)-min(baseImg))*(mask-min(mask))/(max(mask)-min(mask)))
     } else if (is.numeric(scaling)) { # constant scaling
-        scaledNM <- (NM + scaling)/(2 * scaling)
-        if (max(scaledNM) > 1 | min(scaledNM) < -1) warning("Constant is too low! Adjust.")
+        scaledMask <- (mask + scaling)/(2 * scaling)
+        if (max(scaledMask) > 1 | min(scaledMask) < -1) warning("Constant is too low! Adjust.")
     } else if (is.null(scaling)) { # No scaling
-        scaledNM <- NM
+        scaledMask <- mask
     }
 
-    return(scaledNM)
+    return(scaledMask)
 }
 
 #' @title Generate Classification Image (CI)
-#' @description Generate the combinaed of the noise mask (NM) and the base image.
-#' @inheritParams genNM
-#' @param NM Noise mask as generated from \code{\link{genNM}}.
+#' @description Generate the combinaed of the noise mask (mask) and the base image.
+#' @inheritParams genMask
+#' @param mask Mask as generated from \code{\link{genMask}}. If `NULL` (default), then the `mask`
+#'   will be generated automatically by calling \code{\link{genMask}}. See `...`.
 #' @param filename String specifying the name of the ouput CI. Default is `"combined.png"`.
 #' @param outpath String specifying the output target path.
 #' @param resize Scalar specifying if the image should be resized.
 #' @param preview Logical indicating if the iamge should be previewed in the Viewer Panel.
-#' @param ... List of parameters passed to \code{\link{genNM}}. If `NM` is provided, then ellipsis
+#' @param ... List of parameters passed to \code{\link{genMask}}. If `mask` is provided, then ellipsis
 #'   will be ignored.
 #' @return NULL
 #' @examples NULL
 #' @export genCI
-genCI <- function(NM = NULL, filename = "combined.png", outpath = "./cis",
+genCI <- function(mask = NULL, filename = "combined.png", outpath = "./cis",
                   resize = NULL, preview = FALSE, ...) {
 
     # Retrieve arguments passed
     dots <- list(...)
 
     # Read base image
-    if (is.character(dots$baseImg)) baseImg <- readBaseImg(dots$baseImg)
+    if (is.character(dots$baseImg)) dots$baseImg <- readBaseImg(dots$baseImg)
 
-    # Noise mask (NM)
-    if (is.null(NM)) NM <- genNM(...)
+    # Generate mask
+    if (is.null(mask)) mask <- genMask(...)
 
     # Write and save combined image
-    combined <- (baseImg + NM) / 2
+    baseImg <- dots$baseImg
+    combined <- (baseImg + mask) / 2
     combined <- matrix(combined, nrow = 512)
     if (!dir.exists(outpath)) dir.create(outpath)
     imgPath <- file.path(outpath, filename)
@@ -98,9 +100,7 @@ genCI <- function(NM = NULL, filename = "combined.png", outpath = "./cis",
         magick::image_scale(resize) %>%
         magick::image_write(path = imgPath, format = "png")
 
-    # Print image
+    # Preview image
     if (preview) print( magick::image_read(imgPath) )
-
-    return(combined)
 }
 
