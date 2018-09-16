7275e4c22bd17de998a8b4cb336197ed62146e6b
mathi <mathias.schmitz@outlook.com>
2018-09-16 16:29:04 +0200

    small bug fix

--- R/test.R
+++ R/test.R
@@ -2,21 +2,21 @@
 # Script to test package
 
 # Setup --------------------------------------------------------------------------------------------
-library(data.table)
-library(magrittr)
-library(here)
-
-source("R/RC.R")
-source("R/dataWranglingCleaning.R")
-source("R/stats.R")
-
-load("data/N.rda")
-load("data/RC.rda"); setDT(RC)
-
-
-RC[, genCI(response    = responses,
-           stim        = stimuli,
-           noiseMatrix = N,
-           baseImg     = "data/base.jpeg",
-           outputPath  = paste0("cis/", id, ".png") %>% unique), id]
+# library(data.table)
+# library(magrittr)
+# library(here)
+#
+# source("R/RC.R")
+# source("R/dataWranglingCleaning.R")
+# source("R/stats.R")
+#
+# load("data/N.rda")
+# load("data/RC.rda"); setDT(RC)
+#
+#
+# RC[, genCI(response    = responses,
+#            stim        = stimuli,
+#            noiseMatrix = N,
+#            baseImg     = "data/base.jpeg",
+#            outputPath  = paste0("cis/", id, ".png") %>% unique), id]
 
