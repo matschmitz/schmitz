ae05d80a5e06809ac5d93bfcb0be1371b6534009
mathi <mathias.schmitz@outlook.com>
2018-10-02 16:01:00 +0200

    m_sd ...

--- R/stats.R
+++ R/stats.R
@@ -5,4 +5,4 @@
 #' @return formated string
 #' @examples m_sd(1:3)
 #' @export m_sd
-m_sd <- function(x, ...) sprintf("%.2f(%.2f)", mean(x), sd(x))
+m_sd <- function(x, ...) sprintf("%.2f(%.2f)", mean(x, ...), sd(x, ...))
