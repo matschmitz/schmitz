117c8001970caa22bf394f9652ec980fce8ce3a1
matschmitz <37771001+matschmitz@users.noreply.github.com>
2018-10-31 13:45:04 +0100

    added ss

--- R/stats.R
+++ R/stats.R
@@ -1,3 +1,7 @@
+#' @import data.table
+#' @import magrittr
+#' @importFrom lmSupport modelEffectSizes
+
 #' @title Formated mean and standard deviation
 #'
 #' @description Format the mean and sd of a vector
@@ -6,3 +10,19 @@
 #' @examples m_sd(1:3)
 #' @export m_sd
 m_sd <- function(x, ...) sprintf("%.2f(%.2f)", mean(x, ...), sd(x, ...))
+
+#' @title Effect sizes for lm summary
+#'
+#' @description Add effect sizes to the lm summary
+#' @param mdl an lm model obtained from lm
+#' @param digits number of decimal digits
+#' @return data table with summary statistics
+#' @examples ss(mdl)
+#' @export ss
+ss <- function(mdl, digits = 3) {
+    mdl.coefficients <- summary(mdl)$coefficients
+    mdl.effects <- lmSupport::modelEffectSizes(mdl, Print = FALSE)$Effects[, 2:3]
+    mdl.s <- data.table(cbind(mdl.coefficients, mdl.effects))
+    mdl.s <- schmitz::roundify(mdl.s, digits)
+    return(mdl.s)
+}
